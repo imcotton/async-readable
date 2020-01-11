7f1f5baad84bccb22e3e4933b20313601ae526f9
Cotton Hou <himcotton@gmail.com>
2020-01-11 20:29:02 +0800

    off removes error listener

--- lib/async-readable.ts
+++ lib/async-readable.ts
@@ -79,6 +79,7 @@ export function asyncReadable <T extends Buffer> (stream: ReadableStream) {
 
     function off () {
         stream.off('readable', onReadable);
+        stream.off('error', reject);
     }
 
     async function* gen () {
