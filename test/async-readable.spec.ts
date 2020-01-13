5873419eebab18eed85d1a77e9778e20d14dc495
Cotton Hou <himcotton@gmail.com>
2020-01-13 21:00:12 +0800

    tests

--- test/async-readable.spec.ts
+++ test/async-readable.spec.ts
@@ -1,4 +1,8 @@
-import { asyncReadable } from '../lib/async-readable';
+import { Readable } from 'stream';
+
+import { mirror } from 'proxy-bind';
+
+import { asyncReadable, toReadableStream, AsyncReadable } from '../lib/async-readable';
 
 
 
@@ -6,8 +10,75 @@ import { asyncReadable } from '../lib/async-readable';
 
 describe('AsyncReadable', () => {
 
-    test('import', () => {
-        expect(asyncReadable).toBeDefined();
+    const source = Buffer.from([ 1, 2, 3, 4, 5 ]);
+
+    let read: (size: number) => Promise<Buffer>;
+
+
+
+    beforeEach(() => {
+
+        const [ readable, { push } ] = mirror(new Readable({ read () {} }));
+
+        read = asyncReadable(readable).read;
+
+        push(source.slice(0, 2));
+
+        setTimeout(() => push(source.slice(2)), 10);
+
+    });
+
+
+
+    test('async', async () => {
+
+        const [ three, two ] = await Promise.all([ read(3), read(2) ]);
+
+        expect(three.length).toBe(3);
+        expect(two.length).toBe(2);
+
+        expect(Buffer.concat([ three, two ])).toEqual(source);
+
+    });
+
+    test.each([ NaN, 0, -1 ])('err: %p', size => {
+
+        expect(read(size)).rejects.toThrowError(RangeError);
+
+    });
+
+});
+
+
+
+describe('toReadableStream', () => {
+
+    const source = Buffer.from([ 1, 2, 3, 4, 5 ]);
+
+    const readable = new Readable({
+        read () {
+            this.push(source);
+            this.push(null);
+        },
+    })
+
+    const gen = async function* ({ read }: AsyncReadable) {
+        yield read(3);
+        yield read(2);
+    };
+
+    const stream = toReadableStream(gen)(readable);
+
+    test('', async () => {
+
+        const list = [];
+
+        for await (const item of stream) {
+            list.push(item);
+        }
+
+        expect(Buffer.concat(list)).toEqual(source);
+
     });
 
 });
